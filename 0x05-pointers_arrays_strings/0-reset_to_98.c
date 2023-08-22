83cd33bc65750b358100cfcff8dc600ab255e738
jaguhdeveloper@gmail.com <geraldwainaina25@gmail.com>
2023-08-22 22:29:45 +0300

    low level programming

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* update the parameter
+* to 98
+* int n = 98
+*/
+void reset_to_98(int *n)
+{
+     *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* swap function
+*return 0
+*/
+void swap_int(int *a, int *b)
+{
+     int c;
+     
+     c = *a;
+    *a = *b;
+    *b = c;
+}
